314b649d3daf058410ad648e132c239d79f8409a
shelep-olya <shelep.olya@gmail.com>
2024-11-19 19:30:05 +0200

    added basic architecture setup

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 const app = express();
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`App is running on port: ${PORT}`);
-});
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
