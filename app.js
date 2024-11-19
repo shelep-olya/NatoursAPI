11b255152fc94107f42486249c45229ceb24216a
shelep-olya <shelep.olya@gmail.com>
2024-11-19 20:25:16 +0200

    connected to db; configured eslint

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+
 const app = express();
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
 
-const PORT = 3000;
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.MONGO_DB_URI.replace('<PASSWORD>', process.env.PASSWORD);
+mongoose.connect(DB).then(() => {
+  console.log('DB connected successfully!');
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`App is running on port: ${PORT}`);
