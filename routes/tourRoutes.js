058b4aec6e42423851b6de82ae6a3f292a40e054
shelep-olya <shelep.olya@gmail.com>
2024-11-22 23:50:13 +0200

    implemented tourModel & tourController

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,4 +3,15 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
+
+router
+  .route('/:id')
+  .get(tourController.getTourById)
+  .put(tourController.updateTour)
+  .delete(tourController.deleteTour);
+
 module.exports = router;
