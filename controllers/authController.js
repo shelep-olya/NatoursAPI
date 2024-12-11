8ec8033ef4783dd957a3294a27561922e831723b
shelep-olya <shelep.olya@gmail.com>
2024-12-11 11:33:23 +0200

    implement restrictTo middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -68,19 +68,30 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   const decoded = await jwt.verify(token, process.env.JWT_SECRET);
 
-  const freshUser = await User.findById(decoded.id);
-  if (!freshUser) {
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
     return next(
       new AppError('User belonging to this token does no longer exist.', 401)
     );
   }
 
-  if (freshUser.changePasswordAfter(decoded.iat)) {
+  if (currentUser.changePasswordAfter(decoded.iat)) {
     return next(
       new AppError('User recently changed password! Please log in again.', 401)
     );
   }
 
-  req.user = freshUser;
+  req.user = currentUser;
   next();
 });
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You don't have permission to perform this action.", 403)
+      );
+    }
+    next();
+  };
+};
--- models/userModel.js
+++ models/userModel.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema({
     validate: [validator.isEmail, 'User must have a valid email.']
   },
   photo: String,
+  role: {
+    type: String,
+    enum: ['user', 'admin', 'guide', 'lead-guide'],
+    default: 'user'
+  },
   password: {
     type: String,
     required: [true, 'User must have a password.'],
