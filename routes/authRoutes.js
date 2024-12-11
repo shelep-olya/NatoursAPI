6e7ab0ee6e9ef6d104264a226fe4fe040f8a6acf
shelep-olya <shelep.olya@gmail.com>
2024-12-11 12:13:25 +0200

    implement sending email for password reset

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,5 +7,6 @@ router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
+// router.patch('/resetPassword', authController.resetPassword);
 
 module.exports = router;
