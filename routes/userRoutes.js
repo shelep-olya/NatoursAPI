a11b0a2ea4c2b8c361118abf4503bb85a4099614
shelep-olya <shelep.olya@gmail.com>
2024-12-11 13:18:09 +0200

    implement updateMe functionallity

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
 router.route('/').get(userController.getAllUsers);
+
+router.patch('/updateMe', authController.protect, userController.updateMe);
 
 module.exports = router;
