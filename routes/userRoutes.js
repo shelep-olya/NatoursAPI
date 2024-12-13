fcbfcea99ff42f6778139d9770b5885488159628
shelep-olya <shelep.olya@gmail.com>
2024-12-13 13:55:33 +0200

    add deleteMe functionallity

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.route('/').get(userController.getAllUsers);
 
 router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
 module.exports = router;
