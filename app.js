089190b3a884102eacecb12b8c5bb10acd28b3cf
shelep-olya <shelep.olya@gmail.com>
2024-12-17 18:03:01 +0200

    create base pug template

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -13,6 +14,10 @@ const authRouter = require('./routes/authRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(helmet());
 
@@ -44,7 +49,13 @@ app.use(
     ]
   })
 );
-app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The forest hiker',
+    user: 'Olya'
+  });
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
